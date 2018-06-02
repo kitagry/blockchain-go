a703ec35477063dcb9c99a52cafc10c7a3ff94fe
kitagawa <kitadrum50@gmail.com>
2018-06-02 10:22:21 +0900

    Make Blockchain

---
+++ block.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	TimeStamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, PrevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// ブロックチェーンの最初のブロック
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+// ブロックチェーンを最初に生み出すメソッド
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
