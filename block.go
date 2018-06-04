c4ea8636f32a0c675b8e118836383602ac560862
kitagawa <kitadrum50@gmail.com>
2018-06-04 12:39:34 +0900

    Chapter4: transaction

--- block.go
+++ block.go
@@ -3,34 +3,27 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"log"
-	"strconv"
 	"time"
-
-	"github.com/boltdb/bolt"
 )
-
-const blocksBucket = "blocksBucket"
-const dbFile = "my.db"
 
 type Block struct {
 	TimeStamp     int64
-	Data          []byte
+	Transactions  []*Transaction
 	PrevBlockHash []byte
 	Hash          []byte
 	Nonce         int
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
 
-	b.Hash = hash[:]
-}
+// 	b.Hash = hash[:]
+// }
 
-func NewBlock(data string, PrevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}, 0}
+func NewBlock(transactions []*Transaction, PrevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), transactions, PrevBlockHash, []byte{}, 0}
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
 
@@ -40,104 +33,19 @@ func NewBlock(data string, PrevBlockHash []byte) *Block {
 	return block
 }
 
-type Blockchain struct {
-	tip []byte
-	db  *bolt.DB
-}
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
 
-func (bc *Blockchain) AddBlock(data string) {
-	var lastHash []byte
-
-	err := bc.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		lastHash = b.Get([]byte("l"))
-
-		return nil
-	})
-
-	newBlock := NewBlock(data, lastHash)
-
-	err = bc.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		err := b.Put(newBlock.Hash, newBlock.Serialize())
-		err = b.Put([]byte("l"), newBlock.Hash)
-		bc.tip = newBlock.Hash
-
-		if err != nil {
-			log.Panic(err)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
 	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
 }
 
 // ブロックチェーンの最初のブロック
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
-}
-
-// ブロックチェーンを最初に生み出すメソッド
-func NewBlockchain() *Blockchain {
-	var tip []byte
-	db, err := bolt.Open(dbFile, 0600, nil)
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-
-		if b == nil {
-			genesis := NewGenesisBlock()
-			b, err := tx.CreateBucket([]byte(blocksBucket))
-			err = b.Put(genesis.Hash, genesis.Serialize())
-			err = b.Put([]byte("l"), genesis.Hash)
-			if err != nil {
-				log.Panic(err)
-			}
-			tip = genesis.Hash
-		} else {
-			tip = b.Get([]byte("l"))
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bc := Blockchain{tip, db}
-	return &bc
-}
-
-type BlockchainIterator struct {
-	currentHash []byte
-	db          *bolt.DB
-}
-
-func (bc *Blockchain) Iterator() *BlockchainIterator {
-	bci := &BlockchainIterator{bc.tip, bc.db}
-
-	return bci
-}
-
-func (i *BlockchainIterator) Next() *Block {
-	var block *Block
-
-	err := i.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		encodedBlock := b.Get(i.currentHash)
-		block = DeserializeBlock(encodedBlock)
-
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	i.currentHash = block.PrevBlockHash
-
-	return block
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{})
 }
